68a4e55774cf3ae2a8e46a4116932fd8e4f0cc6b
AndreaYpmY <andrea.tocci151001@gmail.com>
2022-10-28 16:04:07 +0200

    Commit funzione stringToIntConverter

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+        FunnyAlgorithms f=new FunnyAlgorithms();
+        f.stringToIntConverter("19");
+    }
+}
